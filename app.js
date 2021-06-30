401430bd73830b986da5da2a3602fb18b4dc8d9e
perex125 <perexlito@gmail.com>
2021-06-30 01:21:50 -0300

    Modificaciones generales

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mercadopago=require('mercadopago');
 
 mercadopago.configure({
     access_token:'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398', 
-    integrator_id: 'APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626',
+    integrator_id: 'dev_24c65fb163bf11ea96500242ac130004',
 });
 
 app.get('/pending', function (req, res) {
@@ -29,7 +29,9 @@ app.get('/', function (req, res) {
 });
 
 
-app.post('/checkout',function (req,res) {
+app.post('/checkout/new',function (req,res) {
+
+ const { name, price, unit, img } = req.query; 
   let preference ={
 
     back_urls: {
@@ -79,13 +81,13 @@ app.post('/checkout',function (req,res) {
 
     items:[
     {
-        id:"1234",
+        id:1234,
         category_id: "1234",  
         picture_url: 'https://perex125-mp-ecommerce-nodejs.herokuapp.com/assets/samsung-galaxy-s9-xxl.jpg',
         description: 'Dispositivo móvil de Tienda e-commerce',
-        title:'Samsung Galaxy S9',
-        unit_price:"15000",
-        quantity:1,
+        title:name,
+        unit_price:parseFloat(price),
+        quantity:parseInt(unit),
 
     }
     ]
@@ -94,7 +96,6 @@ app.post('/checkout',function (req,res) {
 
 mercadopago.preferences.create(preference)
     .then(function(response){
-        console.log(response);
         res.redirect(response.body.init_point);
     }).catch(function(error){
         console.log(error);
